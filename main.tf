ba3c5ff0946acd4b9f0a4161ef7812974a278e5d
root <root@mail.talebi.ca>
2022-07-20 02:15:25 +0000

    terrafrom vpc module created with subnets and routetables in it

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
-provider "aws" {
-  region = "us-east-1"
+module "vpcmodule" {
+      source = "./module/vpc"
+      vpc_cidr = var.vpc_cidr
+      vpc_name = var.vpc_name
+      IGW_name = var.IGW_name
+      routing_table_public_name = var.routing_table_public_name
 }
