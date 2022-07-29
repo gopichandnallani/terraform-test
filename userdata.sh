e99a944e1e7a38214033a697abf1913766606470
Gopichand <gopicn45@gmail.com>
2022-07-29 22:18:24 +0000

    changes made to the userdata.sh file removed sudo from the commands

--- userdata.sh
+++ userdata.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get install -y nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
-sudo apt-get install unzip
-
-git clone https://github.com/gopichandnallani/webpage-test.git /var/www/html/gopichand
+apt-get update -y
+apt-get install -y nginx
+systemctl start nginx
+systemctl enable nginx
+apt-get install unzip
