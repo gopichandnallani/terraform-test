f5fdff41473e3c8718acbd3c31598d241372061e
Gopichand <nallanigopichand@gmail.com>
2023-02-11 09:09:52 +0000

    lb_listener

--- alb.tf
+++ alb.tf
@@ -39,6 +39,9 @@ resource "aws_lb_listener" "front_end" {
 }
 
 resource "aws_lb_listener" "manual" {
+  load_balancer_arn = aws_lb.test.arn
+  port              = "80"
+  protocol          = "HTTP"
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.manual-tg.arn
